5908590b5c50b8ea2fe53feb7541f14161f60297
Iqbal Utomo <muhlisiqbalutomo@gmail.com>
2024-07-04 15:05:54 +0700

    feat: about and services slicing

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,5 +2,29 @@ import { renderNavbar, renderPage } from "./navbar.js";
 
 document.addEventListener("DOMContentLoaded", (e) => {
   renderNavbar();
-  renderPage("home.html", "PawHub | Home");
+  renderPage("services.html", "PawHub | Home");
+});
+
+// floating button
+var floater = document.getElementById("floater");
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function () {
+  var scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    // Scrolling down
+    floater.style.opacity = "1";
+    floater.style.visibility = "visible";
+  } else {
+    // Scrolling up
+    floater.style.opacity = "0";
+    floater.style.visibility = "hidden";
+  }
+
+  lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+});
+
+floater.addEventListener("click", function () {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 });
